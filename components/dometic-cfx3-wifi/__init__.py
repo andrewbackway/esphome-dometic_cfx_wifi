0b82f0eae895d8dd910bb929a680606d7277abcf
Andrew Backway <andrew.backway@outlook.com>
2025-08-14 10:37:42 +1000

    a

--- components/dometic-cfx3-wifi/__init__.py
+++ components/dometic-cfx3-wifi/__init__.py
@@ -7,23 +7,12 @@ DEPENDENCIES = []
 AUTO_LOAD = ["sensor", "binary_sensor", "text_sensor"]
 
 dometic_cfx_wifi_ns = cg.esphome_ns.namespace("dometic-cfx3-wifi")
-DometicCfxWifi = dometic_cfx_wifi_ns.class_("DometicCfxWifi", cg.Component)
+DometicCfxWifi = dometic_cfx_wifi_ns.class_("DometDometicCFXComponenticCfxWifi", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(DometicCfxWifi),
     cv.Required("host"): cv.string,
-    cv.Required("port"): cv.port,
-    cv.Optional("sensor_comp0_temp"): sensor.sensor_schema(),
-    cv.Optional("sensor_comp1_temp"): sensor.sensor_schema(),
-    cv.Optional("sensor_comp0_set_temp"): sensor.sensor_schema(),
-    cv.Optional("sensor_comp1_set_temp"): sensor.sensor_schema(),
-    cv.Optional("sensor_comp0_open_count"): sensor.sensor_schema(),
-    cv.Optional("sensor_comp1_open_count"): sensor.sensor_schema(),
-    cv.Optional("binary_comp0_open"): binary_sensor.binary_sensor_schema(),
-    cv.Optional("binary_comp1_open"): binary_sensor.binary_sensor_schema(),
-    cv.Optional("sensor_dc_voltage"): sensor.sensor_schema(),
-    cv.Optional("sensor_dc_current"): sensor.sensor_schema(),
-    cv.Optional("text_serial"): text_sensor.text_sensor_schema(),
+    cv.Optional("port"): cv.port
 }).extend(cv.COMPONENT_SCHEMA)
 
 
@@ -32,47 +21,3 @@ async def to_code(config):
     await cg.register_component(var, config)
     cg.add(var.set_host(config["host"]))
     cg.add(var.set_port(config["port"]))
-
-    if "sensor_comp0_temp" in config:
-        sens = await sensor.new_sensor(config["sensor_comp0_temp"])
-        cg.add(var.set_sensor_comp0_temp(sens))
-
-    if "sensor_comp1_temp" in config:
-        sens = await sensor.new_sensor(config["sensor_comp1_temp"])
-        cg.add(var.set_sensor_comp1_temp(sens))
-
-    if "sensor_comp0_set_temp" in config:
-        sens = await sensor.new_sensor(config["sensor_comp0_set_temp"])
-        cg.add(var.set_sensor_comp0_set_temp(sens))
-
-    if "sensor_comp1_set_temp" in config:
-        sens = await sensor.new_sensor(config["sensor_comp1_set_temp"])
-        cg.add(var.set_sensor_comp1_set_temp(sens))
-
-    if "sensor_comp0_open_count" in config:
-        sens = await sensor.new_sensor(config["sensor_comp0_open_count"])
-        cg.add(var.set_sensor_comp0_open_count(sens))
-
-    if "sensor_comp1_open_count" in config:
-        sens = await sensor.new_sensor(config["sensor_comp1_open_count"])
-        cg.add(var.set_sensor_comp1_open_count(sens))
-
-    if "binary_comp0_open" in config:
-        bs = await binary_sensor.new_binary_sensor(config["binary_comp0_open"])
-        cg.add(var.set_binary_comp0_open(bs))
-
-    if "binary_comp1_open" in config:
-        bs = await binary_sensor.new_binary_sensor(config["binary_comp1_open"])
-        cg.add(var.set_binary_comp1_open(bs))
-
-    if "sensor_dc_voltage" in config:
-        sens = await sensor.new_sensor(config["sensor_dc_voltage"])
-        cg.add(var.set_sensor_dc_voltage(sens))
-
-    if "sensor_dc_current" in config:
-        sens = await sensor.new_sensor(config["sensor_dc_current"])
-        cg.add(var.set_sensor_dc_current(sens))
-
-    if "text_serial" in config:
-        ts = await text_sensor.new_text_sensor(config["text_serial"])
-        cg.add(var.set_text_serial(ts))
--- components/dometic-cfx3-wifi/dometic-cfx3-wifi.h
+++ components/dometic-cfx3-wifi/dometic-cfx3-wifi.h
@@ -1,91 +1,42 @@
-#ifndef DOMETIC_CFX_WIFI_H
-#define DOMETIC_CFX_WIFI_H
+#pragma once
 
-#include "esphome.h"
-#include <WiFiClient.h>
-#include <ArduinoJson.h>
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include <lwip/sockets.h>
+#include <string>
 
-class DometicCfxWifi : public Component {
- private:
-  static const char* DDMP_KEY;
-  static const char* CARRIAGE_RETURN;
-  static const char* LOG_TAG;
-  static const uint32_t RECONNECT_INTERVAL_MS = 3600000UL;  // 1 hour
-  static const uint32_t RETRY_DELAY_MS = 5000;              // 5 seconds
+namespace esphome {
+namespace cfx3 {
 
+class Cfx3Component : public Component {
+ public:
+  // User config
   std::string host_;
   int port_;
-  WiFiClient client_;
-  unsigned long connected_at_ = 0;
-  std::string buffer_;
-  enum State { DISCONNECTED, WAIT_NOP, SENT_PING, SUBSCRIBED };
-  State state_ = DISCONNECTED;
 
-  float compartment_0_temp_ = NAN;
-  float compartment_1_temp_ = NAN;
-  float compartment_0_set_temp_ = NAN;
-  float compartment_1_set_temp_ = NAN;
-  float dc_voltage_ = NAN;
-  float dc_current_ = NAN;
-  bool compartment_0_open_ = false;
-  bool compartment_0_open_previous_ = false;
-  int compartment_0_open_count_ = 0;
-  bool compartment_1_open_ = false;
-  bool compartment_1_open_previous_ = false;
-  int compartment_1_open_count_ = 0;
-  std::string product_serial_ = "";
-
-  // Sensor pointers
-  sensor::Sensor *sensor_comp0_temp_{nullptr};
-  sensor::Sensor *sensor_comp1_temp_{nullptr};
-  sensor::Sensor *sensor_comp0_set_temp_{nullptr};
-  sensor::Sensor *sensor_comp1_set_temp_{nullptr};
-  sensor::Sensor *sensor_comp0_open_count_{nullptr};
-  sensor::Sensor *sensor_comp1_open_count_{nullptr};
-  binary_sensor::BinarySensor *binary_comp0_open_{nullptr};
-  binary_sensor::BinarySensor *binary_comp1_open_{nullptr};
-  sensor::Sensor *sensor_dc_voltage_{nullptr};
-  sensor::Sensor *sensor_dc_current_{nullptr};
-  text_sensor::TextSensor *text_serial_{nullptr};
-
-  void send_action(int action);
-  void send_sub(const uint8_t param[4]);
-  void subscribe_topics();
-  void process_publish(JsonArray &arr);
-  void process_message();
-
- public:
-  void set_host(std::string host) { host_ = host; }
-  void set_port(int port) { port_ = port; }
-
-  // Setters for sensors (called from __init__.py)
-  void set_sensor_comp0_temp(sensor::Sensor *s) { sensor_comp0_temp_ = s; }
-  void set_sensor_comp1_temp(sensor::Sensor *s) { sensor_comp1_temp_ = s; }
-  void set_sensor_comp0_set_temp(sensor::Sensor *s) { sensor_comp0_set_temp_ = s; }
-  void set_sensor_comp1_set_temp(sensor::Sensor *s) { sensor_comp1_set_temp_ = s; }
-  void set_sensor_comp0_open_count(sensor::Sensor *s) { sensor_comp0_open_count_ = s; }
-  void set_sensor_comp1_open_count(sensor::Sensor *s) { sensor_comp1_open_count_ = s; }
-  void set_binary_comp0_open(binary_sensor::BinarySensor *b) { binary_comp0_open_ = b; }
-  void set_binary_comp1_open(binary_sensor::BinarySensor *b) { binary_comp1_open_ = b; }
-  void set_sensor_dc_voltage(sensor::Sensor *s) { sensor_dc_voltage_ = s; }
-  void set_sensor_dc_current(sensor::Sensor *s) { sensor_dc_current_ = s; }
-  void set_text_serial(text_sensor::TextSensor *t) { text_serial_ = t; }
-
-  // Getters
-  float get_compartment_0_temp() { return compartment_0_temp_; }
-  float get_compartment_1_temp() { return compartment_1_temp_; }
-  float get_compartment_0_set_temp() { return compartment_0_set_temp_; }
-  float get_compartment_1_set_temp() { return compartment_1_set_temp_; }
-  float get_dc_voltage() { return dc_voltage_; }
-  float get_dc_current() { return dc_current_; }
-  bool get_compartment_0_open() { return compartment_0_open_; }
-  bool get_compartment_1_open() { return compartment_1_open_; }
-  int get_compartment_0_open_count() { return compartment_0_open_count_; }
-  int get_compartment_1_open_count() { return compartment_1_open_count_; }
-  std::string get_product_serial() { return product_serial_; }
-
+  // Sensors
+  // TOOD UPDATE WITH ACTUAL LIST
+  
   void setup() override;
   void loop() override;
+  void dump_config() override;
+
+  void set_host(const std::string &host) { host_ = host; }
+  void set_port(int port) { port_ = port; }
+
+ protected:
+  int sockfd_ = -1;
+  uint64_t last_connect_attempt_ = 0;
+  uint64_t reconnect_delay_ms_ = 1000; // Start at 1 sec
+
+  std::string rx_buffer_;
+
+  bool connect_socket_();
+  void close_socket_();
+  void handle_incoming_data_();
+  void parse_and_publish_(const std::string &json_str);
 };
 
-#endif  // DOMETIC_CFX_WIFI_H
+}  // namespace cfx3
+}  // namespace esphome
