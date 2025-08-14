674e96dcc89e585739c87f04c39199002e8e836b
Andrew Backway <andrew.backway@outlook.com>
2025-08-14 10:42:28 +1000

    a

--- components/dometic-cfx3-wifi/dometic-cfx3-wifi.h
+++ components/dometic-cfx3-wifi/dometic-cfx3-wifi.h
@@ -13,11 +13,12 @@ class DometicCFXComponent : public Component {
   void set_host(const std::string &host) { host_ = host; }
   void set_port(uint16_t port) { port_ = port; }
 
-  #void set_compartment_temp_sensor(sensor::Sensor *sensor) { compartment_temp_ = sensor; }
-  #void set_ambient_temp_sensor(sensor::Sensor *sensor) { ambient_temp_ = sensor; }
-  #void set_voltage_sensor(sensor::Sensor *sensor) { voltage_ = sensor; }
-  #void set_door_status_sensor(sensor::Sensor *sensor) { door_status_ = sensor; }
-
+  /*
+  void set_compartment_temp_sensor(sensor::Sensor *sensor) { compartment_temp_ = sensor; }
+  void set_ambient_temp_sensor(sensor::Sensor *sensor) { ambient_temp_ = sensor; }
+  void set_voltage_sensor(sensor::Sensor *sensor) { voltage_ = sensor; }
+  void set_door_status_sensor(sensor::Sensor *sensor) { door_status_ = sensor; }
+  */
   void setup() override;
   void loop() override;
 
@@ -30,11 +31,9 @@ class DometicCFXComponent : public Component {
   void request_data_();
   void parse_response_(const std::vector<uint8_t> &data);
 
-  #sensor::Sensor *compartment_temp_ = nullptr;
-  #sensor::Sensor *ambient_temp_ = nullptr;
-  #sensor::Sensor *voltage_ = nullptr;
-  #sensor::Sensor *door_status_ = nullptr;
-};
-
-}  // namespace dometic_cfx
-}  // namespace esphome
+  /*
+  sensor::Sensor *compartment_temp_ = nullptr;
+  sensor::Sensor *ambient_temp_ = nullptr;
+  sensor::Sensor *voltage_ = nullptr;
+  sensor::Sensor *door_status_ = nullptr;
+*/
