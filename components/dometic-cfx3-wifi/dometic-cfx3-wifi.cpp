c4b620bfc96ad13637f3d8a6033309b039beffa9
Andrew Backway <andrew.backway@outlook.com>
2025-08-14 19:41:11 +1000

    a

--- components/dometic-cfx3-wifi/dometic-cfx3-wifi.cpp
+++ components/dometic-cfx3-wifi/dometic-cfx3-wifi.cpp
@@ -265,15 +265,6 @@ void DometicCFXComponent::poll_recv_() {
   char buf[128];
   // recv() will return -1 with EWOULDBLOCK after ~timeout or 0 on close
   ssize_t n = ::recv(this->sock_, buf, sizeof(buf), 0);
-  
-  if (n == -1) {
-    ESP_LOGW(TAG, "recv failed: errno=%d (%s)", errno, strerror(errno));
-  } else if (n == 0) {
-    ESP_LOGW(TAG, "Connection closed by peer");
-  } else {
-    ESP_LOGD(TAG, "Received %zd bytes", n);
-  }
-
   if (n == 0) {
     ESP_LOGW(TAG, "Peer closed connection");
     this->close_();
